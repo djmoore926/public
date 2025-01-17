b7e686a92a464e4442eedc508c78ed15ce5a0aab
djmoore926 <djmoore926@gmail.com>
2025-01-17 14:28:39 -0500

    Update test_extract_markdown_images.py

--- src/test_extract_markdown_images.py
+++ src/test_extract_markdown_images.py
@@ -1,5 +1,5 @@
 import unittest
-from extract_markdown_images import extract_markdown_images
+from extract_markdown_images_links import extract_markdown_images, extract_markdown_links
 
 class TestExtractMarkdownImages(unittest.TestCase):
     def test_single(self):
@@ -10,12 +10,33 @@ class TestExtractMarkdownImages(unittest.TestCase):
     def test_multiple(self):
         text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
         result = extract_markdown_images(text)
-        self.assertEqual(result, [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")])
+        self.assertEqual(result, [
+            ("rick roll", "https://i.imgur.com/aKaOqIh.gif"), 
+            ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")
+            ])
 
     def test_no_images(self):
         text = "This text does not contain any images or anything that we are looking for with our function."
         result = extract_markdown_images(text)
-        self.assertEqual(result, [])
+        self.assertEqual(result, None)
+
+    def test_multiple_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [
+            ("to boot dev", "https://www.boot.dev"), 
+            ("to youtube", "https://www.youtube.com/@bootdotdev")
+            ])
+    
+    def test_no_links(self):
+        text = "This text does not contain any images or anything that we are looking for with our function."
+        result = extract_markdown_links(text)
+        self.assertEqual(result, None)
+
+    def test_incorrect_format(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, None)
 
 if __name__ == "__main__":
     unittest.main()
